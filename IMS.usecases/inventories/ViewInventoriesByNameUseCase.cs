7f4352070ba9326b748192cfcb9b46065ae63fb3
ibra-mh <112883985+ibra-mh@users.noreply.github.com>
2024-10-31 11:33:40 +0100

    Add files via upload

---
+++ IMS.usecases/inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,27 @@
+﻿using IMS.CoreBusiness;
+using IMS.usecases.inventories.Interfaces;
+using IMS.usecases.PluginInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.usecases.inventories
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await inventoryRepository.GetInventoriesByNameAsync(name);
+
+        }
+    }
+}
